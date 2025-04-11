283d3dadb12839b1d6cbd4456277079c1a401395
Victor Modebe <nnamdimodebe@gmail.com>
2025-04-11 09:58:51 -0400

    initial commit

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,20 @@
+#!/usr/bin/env groovy
+package com.example
+
+class Docker implements Serializable {
+
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage (String imageName) {
+        script.echo 'building the the docker image...'
+        script.withCredentials([script.usernamePassword(credentialsId: 'DOCKER-LOGIN', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+            script.sh "docker push $imageName"
+        }
+    }
+}
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,11 +1,6 @@
 #!/usr/bin/env groovy
 
-def call () {
-    echo 'building the the docker image...'
-    withCredentials([usernamePassword(credentialsId: 'DOCKER-LOGIN', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t vicdg8t/my-repo:jma-3.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push vicdg8t/my-repo:jma-3.0'
-    }
+import com.example.Docker
+def call (String imageName) {
+    return new Docker(this).buildDockerImage(imageName)
 }
-
