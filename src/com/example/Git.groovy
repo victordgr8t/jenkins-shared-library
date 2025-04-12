c1c367da54fc40fce46fea3896923139a45c7b60
Victor Modebe <nnamdimodebe@gmail.com>
2025-04-12 00:58:20 -0400

    added git push from jenkins

--- src/com/example/Git.groovy
+++ src/com/example/Git.groovy
@@ -18,7 +18,7 @@ class Git implements Serializable {
                 script.sh 'git branch'
                 script.sh 'git config --list'
 
-                script.sh "git remote set-url origin https://$script.USER}:$script.PASS}@github.com/victordgr8t/java-maven-app.git"
+                script.sh "git remote set-url origin https://${script.USER}:${script.PASS}@github.com/victordgr8t/java-maven-app.git"
                 script.sh 'git add .'
                 script.sh ' git commit -m "ci: version bump"'
                 script.sh 'git push origin HEAD:main'
