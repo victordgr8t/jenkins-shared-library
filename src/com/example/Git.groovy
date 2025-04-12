0e746972d9d4729bd0496ddd0d444eb753252cbc
Victor Modebe <nnamdimodebe@gmail.com>
2025-04-12 11:29:49 -0400

    test commit

--- src/com/example/Git.groovy
+++ src/com/example/Git.groovy
@@ -11,16 +11,16 @@ class Git implements Serializable {
 
     def commitUpdate() {
             script.withCredentials([script.usernamePassword(credentialsId: 'github-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                script.sh 'git config --global user.email "jenkins@example.com"'
-                script.sh 'git config --global user.name "jenkins"'
+                //script.sh 'git config --global user.email "jenkins@example.com"'
+                //script.sh 'git config --global user.name "jenkins"'
 
-                script.sh 'git status'
-                script.sh 'git branch'
-                script.sh 'git config --list'
+                //script.sh 'git status'
+                //script.sh 'git branch'
+                //script.sh 'git config --list'
 
                 script.sh "git remote set-url origin https://${script.USER}:${script.PASS}@github.com/victordgr8t/java-maven-app.git"
                 script.sh 'git add .'
-                script.sh ' git commit -m "ci: version bump"'
+                script.sh 'git commit -m "ci: version bump"'
                 script.sh 'git push origin HEAD:main'
             }
         }
